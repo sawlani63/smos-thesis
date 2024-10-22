ce678b8b0c63f6a65f3f653a694b5c0ca06ff80a
alwin-joshy <joshyalwin@gmail.com>
2024-10-22 11:44:26 +1100

    uart: use tock_registers
    Signed-off-by: alwin-joshy <joshyalwin@gmail.com>

--- crates/root_server/src/uart/uart_meson.rs
+++ crates/root_server/src/uart/uart_meson.rs
@@ -1,29 +1,42 @@
-use smos_common::util::BIT;
 use sel4::sel4_cfg;
+use smos_common::util::BIT;
+use tock_registers::interfaces::{Readable, Writeable};
+use tock_registers::registers::{ReadOnly, ReadWrite};
+use tock_registers::{register_bitfields, register_structs};
 
 #[sel4_cfg(PLAT = "odroidc2")]
-pub const UART_PADDR: usize =  0xc81004c0;
-const UART_CONTROL_TX_ENABLE: u32 = BIT(12) as u32;
-const UART_STATUS_TX_FIFO_FULL: u32 = BIT(21) as u32;
+pub const UART_PADDR: usize = 0xc81004c0;
 
 #[sel4_cfg(PLAT = "odroidc2")]
-#[repr(C)]
-pub struct UART {
-    pub wfifo: u32,
-    pub rfifo: u32,
-    pub ctrl: u32,
-    pub status: u32,
-    pub misc: u32,
-    pub reg5: u32
+register_structs! {
+    pub UART {
+        (0x000 => wfifo: ReadWrite<u32>),
+        (0x004 => rfifo: ReadWrite<u32>),
+        (0x008 => ctrl: ReadWrite<u32, CONTROL::Register>),
+        (0x00c => status: ReadOnly<u32, STATUS::Register>),
+        (0x010 => misc: ReadWrite<u32>),
+        (0x014 => reg5: ReadWrite<u32>),
+        (0x018 => @END),
+    }
 }
 
 #[sel4_cfg(PLAT = "odroidc2")]
+register_bitfields![u32,
+    STATUS [
+        TX_FIFO_FULL 21
+    ],
+    CONTROL [
+        TX_ENABLE 12
+    ],
+];
+
+#[sel4_cfg(PLAT = "odroidc2")]
 pub fn plat_uart_init(uart: &mut UART) {
-    uart.ctrl |= UART_CONTROL_TX_ENABLE;
+    uart.ctrl.write(CONTROL::TX_ENABLE::SET);
 }
 
 #[sel4_cfg(PLAT = "odroidc2")]
 pub fn plat_uart_put_char(uart: &mut UART, c: char) {
-    while uart.status == UART_STATUS_TX_FIFO_FULL {}
-    uart.wfifo = c as u32;
+    while uart.status.is_set(STATUS::TX_FIFO_FULL) {}
+    uart.wfifo.set(c.into());
 }
--- crates/root_server/src/uart/uart_pl011.rs
+++ crates/root_server/src/uart/uart_pl011.rs
@@ -1,48 +1,56 @@
 use core::ptr;
-use smos_common::util::BIT;
 use sel4::sel4_cfg;
+use smos_common::util::BIT;
+use tock_registers::interfaces::{Readable, Writeable};
+use tock_registers::registers::ReadWrite;
+use tock_registers::{register_bitfields, register_structs};
 
 #[sel4_cfg(PLAT = "qemu-arm-virt")]
 pub const UART_PADDR: usize = 0x9000000;
-const PL011_UARTFR_TXFF: u32 = BIT(5) as u32;
 
 #[sel4_cfg(PLAT = "qemu-arm-virt")]
-#[repr(C)]
-pub struct UART {
-    dr: u32,        /* 0x000 Data Register */
-    rsr_ecr: u32,   /* 0x004 Receive Status Register/Error Clear Register */
-    res1: u32,      /* 0x008 Reserved */
-    res2: u32,      /* 0x00c Reserved */
-    res3: u32,      /* 0x010 Reserved */
-    res4: u32,      /* 0x014 Reserved */
-    fr: u32,        /* 0x018 Flag Register */
-    res5: u32,      /* 0x01c Reserved */
-    ilpr: u32,      /* 0x020 IrDA Low-Power Counter Register */
-    ibrd: u32,      /* 0x024 Integer Baud Rate Register */
-    fbrd: u32,      /* 0x028 Fractional Baud Rate Register */
-    lcr_h: u32,     /* 0x02c Line Control Register */
-    tcr: u32,       /* 0x030 Control Register */
-    ifls: u32,      /* 0x034 Interrupt FIFO Level Select Register */
-    imsc: u32,      /* 0x038 Interrupt Mask Set/Clear Register */
-    ris: u32,       /* 0x03C Raw Interrupt Status Register */
-    mis: u32,       /* 0x040 Masked Interrupt Status Register */
-    icr: u32,       /* 0x044 Interrupt Clear Register */
-    dmacr: u32,     /* 0x048 DMA Control Register */
-    /* The rest of the registers are either reserved or not used */
+// #[repr(C)]
+register_structs! {
+    pub UART {
+        (0x000 => dr: ReadWrite<u32>),                      /* 0x000 Data Register */
+        (0x004 => rsr_ecr: ReadWrite<u32>),                 /* 0x004 Receive Status Register/Error Clear Register */
+        (0x008 => _reserved1),                              /* 0x00c Reserved */
+        (0x018 => fr: ReadWrite<u32, FR::Register>),                      /* 0x018 Flag Register */
+        (0x01c => _reserved2),                              /* 0x01c Reserved */
+        (0x020 => ilpr: ReadWrite<u32>),                    /* 0x020 IrDA Low-Power Counter Register */
+        (0x024 => ibrd: ReadWrite<u32>),                    /* 0x024 Integer Baud Rate Register */
+        (0x028 => fbrd: ReadWrite<u32>),                    /* 0x028 Fractional Baud Rate Register */
+        (0x02c => lcr_h: ReadWrite<u32>),                   /* 0x02c Line Control Register */
+        (0x030 => tcr: ReadWrite<u32>),                     /* 0x030 Control Register */
+        (0x034 => ifls: ReadWrite<u32>),                    /* 0x034 Interrupt FIFO Level Select Register */
+        (0x038 => imsc: ReadWrite<u32, IMSC::Register>),    /* 0x038 Interrupt Mask Set/Clear Register */
+        (0x03c => ris: ReadWrite<u32>),                     /* 0x03C Raw Interrupt Status Register */
+        (0x040 => mis: ReadWrite<u32>),                     /* 0x040 Masked Interrupt Status Register */
+        (0x044 => icr: ReadWrite<u32>),                     /* 0x044 Interrupt Clear Register */
+        (0x048 => dmacr: ReadWrite<u32>),                  /* 0x048 DMA Control Register */
+        /* The rest of the registers are either reserved or not used */
+        (0x04c => @END),
+    }
 }
+
+#[sel4_cfg(PLAT = "qemu-arm-virt")]
+register_bitfields![u32,
+    IMSC [
+        RXIM OFFSET(5) NUMBITS(1) []
+    ],
+    FR [
+        TXFF OFFSET(5) NUMBITS(1) [],
+        RXFE OFFSET(4) NUMBITS(1) [],
+    ],
+];
 
 #[sel4_cfg(PLAT = "qemu-arm-virt")]
 pub fn plat_uart_init(uart: &mut UART) {
-    unsafe {
-        ptr::write_volatile(&mut uart.imsc as *mut u32, 0x50);
-    }
+    uart.imsc.write(IMSC::RXIM::SET);
 }
-
 
 #[sel4_cfg(PLAT = "qemu-arm-virt")]
 pub fn plat_uart_put_char(uart: &mut UART, c: char) {
-    unsafe {
-        while (ptr::read_volatile(&uart.fr as *const u32) & PL011_UARTFR_TXFF) != 0 {};
-        ptr::write_volatile(&mut uart.dr as *mut u32, c as u32)
-    }
+    while uart.fr.is_set(FR::TXFF) {}
+    uart.dr.set(c.into());
 }
